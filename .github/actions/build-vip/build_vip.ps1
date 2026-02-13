f8f3ece7e4350d2df0daf5bc2e1a4db40ae75a2f
Sergio Velderrain <svelderrain_ruiz@hotmail.com>
2026-02-13 14:36:11 -0800

    ci: resolve build_vip version input as year.minor

--- .github/actions/build-vip/build_vip.ps1
+++ .github/actions/build-vip/build_vip.ps1
@@ -116,8 +116,13 @@ if (Test-Path -Path $versionHelper) {
     $repoInfo = Get-LabVIEWVersionInfo -RepoRoot $ResolvedRepoRoot
     $inputProvided = $PSBoundParameters.ContainsKey('LabVIEWVersion') -and $LabVIEWVersion -ne 0
     if ($inputProvided) {
-        $inputInfo = Get-LabVIEWVersionInfo -VersionInput $LabVIEWVersion -RepoRoot $ResolvedRepoRoot
+        $versionInput = [string]$LabVIEWVersion
+        if ($PSBoundParameters.ContainsKey('LabVIEWMinorRevision')) {
+            $versionInput = "{0}.{1}" -f $LabVIEWVersion, $LabVIEWMinorRevision
+        }
+        $inputInfo = Get-LabVIEWVersionInfo -VersionInput $versionInput -RepoRoot $ResolvedRepoRoot
         $LabVIEWVersion = [int]$inputInfo.Year
+        $LabVIEWMinorRevision = [int]$inputInfo.MinorRevision
     } else {
         $LabVIEWVersion = [int]$repoInfo.Year
         Write-Warning "LabVIEWVersion not provided; defaulting to .lvversion ($($repoInfo.Raw))."
